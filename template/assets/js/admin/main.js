0c3b2a6ca556c2dddc86b2d83a24d79dd41db4ca
Công Thoại <dcthoai1023@gmail.com>
2024-06-09 10:28:07 +0700

    add sub modulo backend and frontend

--- template/assets/js/admin/main.js
+++ template/assets/js/admin/main.js
@@ -11,7 +11,7 @@ function validateLogin(){
         rules: [
             Validator.isRequired('#username', 'Vui lòng nhập username hoặc email!'),
             Validator.isRequired('#password', 'Vui lòng nhập mật khẩu của bạn!'),
-            Validator.minLength('#password', 8, 'Vui lòng nhập tối thiểu 8 kí tự!'),
+            Validator.minLength('#password', 4, 'Vui lòng nhập tối thiểu 8 kí tự!'),
         ],
         // onSubmit: function(data){
         //     let user = {
